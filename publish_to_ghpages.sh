2aae225259358a4a1b42a4688ac71c4a601d3a9d
Matt Thomas <thomasoncode@gmail.com>
2017-09-23 21:56:17 -0500

    Updated the script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -15,11 +15,11 @@ rm -rf .git/worktrees/public/
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "Removing existing files"
-rm -rf public/*
-
 echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && cd..
+
+echo "Pubishing changes to github"
+git push origin gh-pages
